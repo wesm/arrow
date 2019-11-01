23f46171433308fec756e705f1dabab451730a52
Wes McKinney <wesm+git@apache.org>
2019-11-01 12:51:57 -0500

    Trying some things, more work needed [skip ci]

--- cpp/cmake_modules/ThirdpartyToolchain.cmake
+++ cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -24,6 +24,16 @@ add_custom_target(toolchain-benchmarks)
 add_custom_target(toolchain-tests)
 
 # ----------------------------------------------------------------------
+# Some libraries need $SDKROOT to be set
+
+if(APPLE AND NOT DEFINED ENV{SDKROOT})
+  execute_process(COMMAND xcrun --show-sdk-path
+    OUTPUT_VARIABLE SDKROOT
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message(STATUS "Xcode SDKROOT to ${SDKROOT}")
+endif()
+
+# ----------------------------------------------------------------------
 # Toolchain linkage options
 
 set(ARROW_RE2_LINKAGE
@@ -1411,25 +1421,32 @@ if(ARROW_JEMALLOC)
       "${CMAKE_CURRENT_BINARY_DIR}/jemalloc_ep-prefix/src/jemalloc_ep/dist/")
   set(JEMALLOC_STATIC_LIB
       "${JEMALLOC_PREFIX}/lib/libjemalloc_pic${CMAKE_STATIC_LIBRARY_SUFFIX}")
+  set(JEMALLOC_CONFIGURE_OPTIONS
+    "AR=${CMAKE_AR}"
+    "CC=${CMAKE_C_COMPILER}"
+    "--prefix=${JEMALLOC_PREFIX}"
+    "--with-jemalloc-prefix=je_arrow_"
+    "--with-private-namespace=je_arrow_private_"
+    "--without-export"
+    # Don't override operator new()
+    "--disable-cxx" "--disable-libdl"
+    # See https://github.com/jemalloc/jemalloc/issues/1237
+    "--disable-initial-exec-tls")
+  
+  if(APPLE)
+    set(ENV_EXTRAS "SDKROOT=${SDKROOT}")
+  endif()
+
   externalproject_add(
     jemalloc_ep
     URL ${JEMALLOC_SOURCE_URL}
     PATCH_COMMAND
       touch doc/jemalloc.3 doc/jemalloc.html
       # The prefix "je_arrow_" must be kept in sync with the value in memory_pool.cc
-    CONFIGURE_COMMAND ./configure
-                      "AR=${CMAKE_AR}"
-                      "CC=${CMAKE_C_COMPILER}"
-                      "--prefix=${JEMALLOC_PREFIX}"
-                      "--with-jemalloc-prefix=je_arrow_"
-                      "--with-private-namespace=je_arrow_private_"
-                      "--without-export"
-                      # Don't override operator new()
-                      "--disable-cxx" "--disable-libdl"
-                      # See https://github.com/jemalloc/jemalloc/issues/1237
-                      "--disable-initial-exec-tls" ${EP_LOG_OPTIONS}
+    CONFIGURE_COMMAND ./configure ${ENV_EXTRAS} ${JEMALLOC_CONFIGURE_OPTIONS}
+                       ${EP_LOG_OPTIONS}
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND ${MAKE} ${MAKE_BUILD_ARGS}
+    BUILD_COMMAND "${ENV_EXTRAS} ${MAKE} ${MAKE_BUILD_ARGS}"
     BUILD_BYPRODUCTS "${JEMALLOC_STATIC_LIB}"
     INSTALL_COMMAND ${MAKE} install)
 

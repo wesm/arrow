2414aba30a947b50d31da94df5cc4ce332d5a768
Wes McKinney <wesm+git@apache.org>
2020-03-19 13:56:55 -0500

    Address various code review comments. Rename IpcOptions to IpcWriteOptions

--- cpp/src/arrow/ipc/metadata_internal.cc
+++ cpp/src/arrow/ipc/metadata_internal.cc
@@ -1024,8 +1024,7 @@ Status GetKeyValueMetadata(const KVVector* fb_metadata,
     metadata->Append(pair->key()->str(), pair->value()->str());
   }
 
-  *out = metadata;
-
+  *out = std::move(metadata);
   return Status::OK();
 }
 
